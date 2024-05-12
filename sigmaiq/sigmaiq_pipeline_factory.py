54155d8fde84c55883ee66a31026a64df5aac719
Rajesh Sharma <sharrajesh@gmail.com>
2024-05-12 11:05:43 -0700

    Until Stephen uncomments insightidr in toml.
    Temporarily disable InsightIDR pipeline
    
    The InsightIDR pipeline has been temporarily disabled within the sigmaiq_pipeline_factory.py file. The necessary import statement and corresponding dictionary entry for the InsightIDR pipeline have been commented out. This is a temporary change until Stephen uncomment this in the pyproject.toml file.

--- sigmaiq/sigmaiq_pipeline_factory.py
+++ sigmaiq/sigmaiq_pipeline_factory.py
@@ -34,7 +34,8 @@ from sigma.pipelines.elasticsearch import (
 )
 
 ## InsightIDR
-from sigma.pipelines.insight_idr import insight_idr_pipeline
+# RS until Stephen uncomments this in pyproject.toml
+# from sigma.pipelines.insight_idr import insight_idr_pipeline
 
 ## Loki
 from sigma.pipelines.loki import loki_grafana_logfmt, loki_promtail_sysmon, loki_okta_system_log
@@ -141,11 +142,12 @@ AVAILABLE_PIPELINES = {
         "display_name": "Zeek Raw JSON",
     },
     # InsightIDR
-    "insightidr": {
-        "description": "InsightIDR Log Entry Query Language (LEQL) Transformations",
-        "pipeline": insight_idr_pipeline(),
-        "display_name": "InsightIDR LEQL",
-    },
+    # RS until Stephen uncomments this in pyproject.toml
+    # "insightidr": {
+    #     "description": "InsightIDR Log Entry Query Language (LEQL) Transformations",
+    #     "pipeline": insight_idr_pipeline(),
+    #     "display_name": "InsightIDR LEQL",
+    # },
     # Loki
     "loki_grafana_logfmt": {
         "description": "Converts field names to logfmt labels used by Grafana",
