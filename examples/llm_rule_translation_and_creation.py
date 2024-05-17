01df9912e3a1fe2832614b4ff640019a11945e47
Stephen Lincoln <stephen.lincoln@attackiq.com>
2024-05-17 13:08:37 -0400

    Added query to Sigma Rule example

--- examples/llm_rule_translation_and_creation.py
+++ examples/llm_rule_translation_and_creation.py
@@ -18,41 +18,76 @@ except Exception as e:
 # %% Create a Sigma Agent Executor, and pass it our Sigma VectorDB
 sigma_agent_executor = create_sigma_agent(sigma_vectorstore=sigma_llm.sigmadb)
 
+print("\n--------\nRULE TRANSLATION\n--------\n")
 # %% RULE TRANSLATION
 # %% Have the agent automatically translate a Sigma rule to a Splunk query with the splunk_cim_dm pipeline
-
-sigma_rule = r"""
-title: whoami Command
-description: Detects a basic whoami commandline execution
-logsource:
-    product: windows
-    category: process_creation
-detection:
-    selection1:
-        - CommandLine|contains: 'whoami.exe'
-    condition: selection1
-"""
-
-user_input = ("Translate the following Sigma rule to a Splunk query using the 'splunk_cim_dm' pipeline: \n\n" +
-              sigma_rule)
+user_input = "Convert this Sigma rule to a Splunk query using the 'splunk_cim_dm' pipeline: \n\n" + \
+             "title: whoami Command\n" + \
+             "description: Detects a basic whoami commandline execution\n" + \
+             "logsource:\n" + \
+             "    product: windows\n" + \
+             "    category: process_creation\n" + \
+             "detection:\n" + \
+             "    selection1:\n" + \
+             "        - CommandLine|contains: 'whoami.exe'\n" + \
+             "    condition: selection1"
 
 answer = sigma_agent_executor.invoke({"input": user_input})
-print("\nRULE TRANSLATION:", end="\n\n")
-print(f"Question:\n {user_input}", end="\n\n")
-print(f"Answer: \n")
+print(f"\n\nQUESTION:\n {user_input}", end="\n\n")
+print("ANSWER: \n")
+print(answer.get('output'), end="\n\n")
+
+# %% RULE SEARCHING
+# %% The agent will find official Sigma rules in the Sigma vector store based on the context of the users input.
+print("\n--------\nRULE SEARCHING\n--------\n")
+
+user_input = ("What Sigma Rule can detect a process creation event where the parent process is word.exe and the child "
+              "process is cmd.exe?")
+
+answer = sigma_agent_executor.invoke({"input": user_input})
+print(f"QUESTION:\n {user_input}", end="\n\n")
+print("ANSWER: \n")
+print(answer.get('output'), end="\n\n")
+
+# %% QUERY TO SIGMA RULE CONVERSION
+# %% If you already have a query and want to convert it back to a Sigma Rule
+print("\n--------\nQUERY TO SIGMA RULE \n--------\n")
+
+user_input = """
+Convert this Microsoft365Defender query into a Sigma Rule:
+DeviceProcessEvents
+| where ((ProcessCommandLine contains "powershell.exe -enc" or ProcessCommandLine contains "cmd.exe /c" or ProcessCommandLine contains "rundll32.exe") or (InitiatingProcessFolderPath endswith "\\explorer.exe" or InitiatingProcessFolderPath endswith "\\svchost.exe")) and (not((ProcessCommandLine contains "schtasks" or ProcessCommandLine contains "tasklist")))
+
+"""
+
+answer = sigma_agent_executor.invoke({"input": user_input})
+print(f"\n\nQUESTION:\n {user_input}", end="\n\n")
+print("ANSWER: \n")
 print(answer.get('output'), end="\n\n")
 
 # %% RULE CREATION
 # %% The agent will take the user input, look up similar Sigma Rules in the Sigma vector store, then create a brand
 # %% new rule based on the context of the users input and the similar Sigma Rules.
+print("\n--------\nRULE CREATION\n--------\n")
 
 user_input = ("Create a Windows process creation Sigma Rule for certutil downloading a file "
               "from definitely-not-malware.com, then translate it to a Microsoft 365 Defender query.")
 
 answer = sigma_agent_executor.invoke({"input": user_input})
-print("\nRULE CREATION:", end="\n\n")
-print(f"Question:\n {user_input}", end="\n\n")
-print(f"Answer: \n")
+print(f"QUESTION:\n {user_input}", end="\n\n")
+print("ANSWER: \n")
 print(answer.get('output'), end="\n\n")
 
 
+# %% RULE CREATION + TRANSLATION
+# %% The agent will take the user input, look up similar Sigma Rules in the Sigma vector store, then create a brand
+# %% new rule based on the context of the users input and the similar Sigma Rules. Then convert it to a M365 query
+print("\n--------\nRULE CREATION + TRANSLATION\n--------\n")
+
+user_input = ("Create a Windows process creation Sigma Rule for certutil downloading a file "
+              "from definitely-not-malware.com, then translate it to a Microsoft 365 Defender query.")
+
+answer = sigma_agent_executor.invoke({"input": user_input})
+print(f"QUESTION:\n {user_input}", end="\n\n")
+print("ANSWER: \n")
+print(answer.get('output'), end="\n\n")
